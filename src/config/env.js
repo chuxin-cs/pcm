5bb757d15944ae6f51e5bd130cf47b494798aa25
chuxin <15115359055@139.com>
2025-06-05 09:26:12 +0800

    chore: 删除项目中的废弃文件和代码
    清理了项目中不再使用的图片、配置文件、组件、API接口、样式文件等冗余代码和资源文件。包括：
    - 删除public目录下的背景图片和favicon
    - 删除src目录下的config、views、utils、store等模块的废弃代码
    - 删除vite配置文件和插件
    - 删除各种主题样式文件
    - 删除测试页面和示例代码
    
    这些文件在项目中已不再使用，删除以减少项目体积和提高可维护性

--- src/config/env.js
+++
@@ -1,2 +0,0 @@
-let baseUrl = import.meta.env.VITE_APP_API
-export { baseUrl }
--- src/mockProdServer.js
+++
@@ -1,8 +0,0 @@
-import { createProdMockServer } from 'vite-plugin-mock/es/createProdMockServer';
-
-import menuModule from '../mock/menu';
-import userModule from '../mock/user';
-import crudModule from '../mock/crud'
-export const setupProdMockServer = () => {
-  createProdMockServer([...menuModule, ...userModule, ...crudModule]);
-}
